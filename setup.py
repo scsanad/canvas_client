585c15c9f575e0b61cee6ce638f9a7832e4d81f4
Csanad <scsanad@gmail.com>
2020-04-14 18:00:54 +0300

    requirements - python 3.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
     ],
     packages=["canvas_client"],
     include_package_data=True,
