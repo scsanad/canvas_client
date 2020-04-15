349695fa67da40f7a8919f037097fcaa1367a0db
Csanad <scsanad@gmail.com>
2020-04-15 10:34:49 +0300

    fix long description

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-import pathlib
+import os
 from setuptools import setup
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
 
 # This call to setup() does all the work
 setup(
     name="canvas_client",
     version="0.1.1",
     description="Canvas LMS client: Download submissions, upload grades and comments from excel",
-    long_desccription=README,
+    long_description=README,
     long_description_content_type="text/markdown",
+    url="https://github.com/scsanad/canvas_client",
     author="Csanad Sandor",
     author_email="scsanad@cs.ubbcluj.ro",
     license="MIT",
@@ -28,7 +30,7 @@ setup(
                         "pyunpack", "xlwt", "xlrd"],
     entry_points={
         "console_scripts": [
-            "canvas_client=canvas_client.__main__:main",
+            "canvas_client=canvas_client.__main__:main"
         ]
     },
 )
